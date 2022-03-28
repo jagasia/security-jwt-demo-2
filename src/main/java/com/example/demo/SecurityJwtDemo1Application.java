12611221e69b4c0fea3c2c479ddddae12908e3f9
jagasia <rjagadeeswaran@Yahoo.com>
2022-03-28 15:04:25 +0530

    first

---
+++ src/main/java/com/example/demo/SecurityJwtDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJwtDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityJwtDemo1Application.class, args);
+	}
+
+}
